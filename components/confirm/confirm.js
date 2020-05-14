50c5bbe46d7c6430863a4ab7c5bc31b1a5836070
debugtheworldbot <istiancz@gmail.com>
2020-05-14 11:18:45 +0800

    完成公共组件的创建

---
+++ components/confirm/confirm.js
@@ -0,0 +1,27 @@
+Component({
+    properties:{
+     visible:{
+        type:Boolean,
+        value:true
+     },
+     placeholder:{
+       type:String,
+       value:""
+     }
+    },
+    data:{
+      value:""
+    },
+    methods:{
+      confirm(){
+        this.triggerEvent('confirm',this.data.value)
+      },
+      cancel(){
+        this.triggerEvent('cancel',this.data.value)
+      },
+      watchValue(event){
+        this.data.value=event.detail.value;
+        
+      }
+    }
+})
