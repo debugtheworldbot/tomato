3fd5cc3642f903ce3015b1ec64167ef642bee962
debugtheworldbot <istiancz@gmail.com>
2020-05-14 22:52:34 +0800

    complete add

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,12 +6,30 @@ Page({
    */
   data: {
     lists:[
+      {id:1,text:"萨达所",finished:true},
+      {id:2,text:"2222",finished:true},
+      {id:3,text:"133",finished:false},
+      {id:4,text:"144",finished:true},
 
     ],
-    visible:true
+    visible:false
+  },
+  showConfirm(event){
+    this.setData({visible:true})
   },
   confirm(event){
-    console.log(event.detail);
+    let content=event.detail
+    if(content){  
+      let todo=[{id:this.data.lists.length + 1,text:content,finished:false}]
+    this.data.lists= todo.concat(this.data.lists)  
+     this.setData({lists:this.data.lists})
+     this.cancel()
+    }
+  },
+  destoryTodo(event){
+    let index=event.currentTarget.dataset.index
+    this.data.lists[index].finished = true
+    this.setData({lists:this.data.lists})
   },
   cancel(event){
    this.setData({ visible : false } ) 
