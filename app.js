3b2063196f43aae7978bc5594d1de4fb6b51dd2d
debugtheworldbot <istiancz@gmail.com>
2020-05-16 22:37:37 +0800

    封装http

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
   globalData: {
-    userInfo: null
+      host: 'https://gp-server.hunger-valley.com',  //照抄
+      app_id: "wxede8918328d0e331",
+      app_secret: "73d055ed6368f7980218a277b1e54616",
+      t_app_id: "q3e5Vb7aaVVx8FupxBaJyeKc",
+      t_app_secret: "LF9ydbKAhEEbxtQMyvdzBGYU"
   }
-})
+ })
