5c6a49c19da70813fe5c9bae8c7fcc76e79cb211
debugtheworldbot <istiancz@gmail.com>
2020-05-17 14:43:46 +0800

    完成登录跳转页面

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 App({
   globalData: {
       host: 'https://gp-server.hunger-valley.com',  //照抄
-      app_id: "wxede8918328d0e331",
-      app_secret: "73d055ed6368f7980218a277b1e54616",
+      app_id: "wxe590c483ae10ce34",
+      app_secret: "939dc55fcf1b399b61e9e57170dceae3",
       t_app_id: "q3e5Vb7aaVVx8FupxBaJyeKc",
       t_app_secret: "LF9ydbKAhEEbxtQMyvdzBGYU"
   }
