e5fd3604e0f5a28ecdabe45fe00aa4b1b92a31d6
debugtheworldbot <istiancz@gmail.com>
2020-05-17 14:49:49 +0800

    ...

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-      host: 'https://gp-server.hunger-valley.com',  //照抄
-      app_id: "wxe590c483ae10ce34",
-      app_secret: "939dc55fcf1b399b61e9e57170dceae3",
-      t_app_id: "q3e5Vb7aaVVx8FupxBaJyeKc",
-      t_app_secret: "LF9ydbKAhEEbxtQMyvdzBGYU"
-  }
- })
