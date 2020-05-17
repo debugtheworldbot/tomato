603b44d2c18fc98b541c4b48f73d36286d0783ac
debugtheworldbot <istiancz@gmail.com>
2020-05-17 16:26:39 +0800

    完成目标页面增删改查功能

--- components/confirm/confirm.js
+++ components/confirm/confirm.js
@@ -1,27 +1,37 @@
 Component({
-    properties:{
-     visible:{
-        type:Boolean,
-        value:true
-     },
-     placeholder:{
-       type:String,
-       value:""
-     }
+  properties: {
+    placeholder: {
+      type: String,
+      value: ""
     },
-    data:{
-      value:""
+    visible: {
+      type: Boolean,
+      value: false
     },
-    methods:{
-      confirm(){
-        this.triggerEvent('confirm',this.data.value)
-      },
-      cancel(){
-        this.triggerEvent('cancel',this.data.value)
-      },
-      watchValue(event){
-        this.data.value=event.detail.value;
-        
+    value: {
+      type: String,
+      value: ""
+    }
+  },
+  data: {
+    _value: ""
+  },
+  lifetimes: {
+    attached() {
+      if (this.properties.value) {
+        this.properties.value = this.data._value
       }
     }
+  },
+  methods: {
+    confirm(){
+      this.triggerEvent('confirm', this.data._value)
+    },
+    cancel(){
+      this.triggerEvent('cancel', this.data._value)
+    },
+    watchValue(event){
+      this.data._value = event.detail.value
+    }
+  }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,38 +1,73 @@
 // pages/home/home.js
+const {http} = require('../../libs/_http.js');
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  updateId: '',
+  updatIndex: '',
   data: {
-    lists:[
-      {id:1,text:"萨达所",finished:true},
-      {id:2,text:"2222",finished:true},
-      {id:3,text:"133",finished:false},
-      {id:4,text:"144",finished:true},
-
-    ],
-    visible:false
+    lists: [],
+    visibleCreateConfirm: false,
+    visibleUpdateConfirm: false,
+    updateContent: "",
+    animationData: {}
   },
-  showConfirm(event){
-    this.setData({visible:true})
+  onShow(){
+    http.get('/todos?completed=false').then(response=>{
+      this.setData({ lists: response.data.resources })
+    })
   },
-  confirm(event){
-    let content=event.detail
-    if(content){  
-      let todo=[{id:this.data.lists.length + 1,text:content,finished:false}]
-    this.data.lists= todo.concat(this.data.lists)  
-     this.setData({lists:this.data.lists})
-     this.cancel()
+  confirmCreate(event){
+    let content = event.detail
+    console.log(content)
+    if (content) {
+      http.post('/todos',{
+          completed: false, description: content
+      })
+      .then(response => {
+        let todo = [response.data.resource]
+        this.data.lists = todo.concat(this.data.lists)
+        this.setData({ lists: this.data.lists })
+        this.hideCreateConfirm()
+      })
     }
   },
-  destoryTodo(event){
-    let index=event.currentTarget.dataset.index
-    this.data.lists[index].finished = true
-    this.setData({lists:this.data.lists})
+  changeText(event){
+    let {content,id,index} = event.currentTarget.dataset
+    this.updateId = id
+    this.updatIndex = index
+    this.setData({ visibleUpdateConfirm: true, updateContent: content})
   },
-  cancel(event){
-   this.setData({ visible : false } ) 
-   
+  confirmUpdate(event){
+    let content = event.detail
+    http.put(`/todos/${this.updateId}`, {
+      description: content
+    })
+    .then(response => {
+      let todo = response.data.resource
+      this.data.lists[this.updatIndex] = todo
+      this.setData({ lists: this.data.lists })
+      this.hideUpdateConfirm()
+    })
+  },
+  destroyTodo(event){
+    let index = event.currentTarget.dataset.index
+    let id = event.currentTarget.dataset.id
+    http.put(`/todos/${id}`,{
+      completed: true
+    })
+    .then(response => {
+      let todo = response.data.resource
+      this.data.lists[index] = todo
+      this.setData({ lists: this.data.lists })
+    })
+  },
+  hideCreateConfirm(){
+    this.setData({ visibleCreateConfirm: false })
+  },
+  showCreateConfirm(){
+    this.setData({ visibleCreateConfirm: true })
+  },
+  hideUpdateConfirm(){
+    this.setData({ visibleUpdateConfirm: false })
   }
 })
